1a140c00e018113d4ebc3bae8f8eab8eaa071a37
sawyerhanlon <sawyerhanlon@hanovernorwichschools.org>
2015-11-30 14:56:06 -0500

    Updated from Brython Server: November 30, 2015 2:56:15 PM EST

--- conway.py
+++ conway.py
@@ -6,8 +6,23 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, Color, Frame
+
+Black = Color(0, 1)
+White = Color(0xffffff, 1)
+
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
+def coordinates(x,y):
+    return([[x-10, y-10], [x-10, y], [x-10, y+10], [x, y-10], [x, y+10], [x+10, y-10], [x+10, y], [x+10, y+10]])
+    
+def
+
 class Cell(Sprite):
-    cell = RectangleAsset(10, 10, 
+    cell = RectangleAsset(10, 10, Black)
+    
+    def__init__(self, position):
+        super().__init__(Cell.cell, position)
+        self.x = x
+        self.y = y
