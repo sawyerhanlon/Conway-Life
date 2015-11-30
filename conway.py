36f9fb09b4391d6b5b78c5d4e80578198b32feeb
sawyerhanlon <sawyerhanlon@hanovernorwichschools.org>
2015-11-30 14:37:19 -0500

    Updated from Brython Server: November 30, 2015 2:37:27 PM EST

--- conway.py
+++ conway.py
@@ -6,3 +6,8 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+class Cell(Sprite):
+    cell = RectangleAsset(10, 10, 
